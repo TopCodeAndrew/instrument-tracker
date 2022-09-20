cea4dad6a40e8ce634abfed2e914216d94900cec
Andrew Smith <topcodeandrew@gmail.com>
2022-09-20 10:08:55 -0600

    sequlize work

--- server/index.js
+++ server/index.js
@@ -3,12 +3,31 @@ const { SERVER_PORT, CONNECTION_STRING } = process.env;
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    },
+});
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/api/instrument", (req, res) => {
-    console.log("endpoint hit");
+app.post("/api/instrument", async (req, res) => {
+    const { name, family, price, imgURL } = req.body;
+    // console.log(name, family, price, imgURL);
+    // console.log("endpoint hit");
+    await sequelize
+        .query(
+            `
+    INSERT INTO instruments (name, family, price, image_url)
+    VALUES ('${name}', ${family}, ${price},'${imgURL}');`
+        )
+        .catch((err) => console.log(err));
     res.status(200).send("hey front end, it worked");
 });
 
